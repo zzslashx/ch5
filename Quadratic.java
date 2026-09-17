98ec00864c44e62ce09adbe2e8add77a4ce68201
𝓥𝓮𝓲𝓵 <2054575@learn.vsb.bc.ca>
2026-09-16 20:47:00 -0700

    Add files via upload

---
+++ Quadratic.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Quadratic {
+	public static void main(String[] args) { 
+		Scanner scanner = new Scanner(System.in);
+		double a=0.0;
+		double b=0.0;
+		double c=0.0;
+		System.out.println("Give the value for a:");
+		a = scanner.nextDouble();
+		System.out.println("Give the value for b:");
+		b = scanner.nextDouble();
+		System.out.println("Give the value for c:");
+		c = scanner.nextDouble();
+		if((Math.pow(b,2)-4*a*c)<0) System.out.println("There is no answer");
+		else if((Math.pow(b,2)-4*a*c)==0) {
+			System.out.printf("x=%.2f",quadratic1(a,b,c));
+			}
+		 else{
+			System.out.printf("x=%.2f and %.2f", quadratic1(a,b,c), quadratic2(a,b,c));
+			}	
+		
+		}
+	public static double quadratic1(double a, double b, double c)	{
+		
+		return ((-b+Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a));
+		
+		}
+		public static double quadratic2(double a, double b, double c)	{
+		
+		return ((-b-Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a));
+		
+		}
+	}
