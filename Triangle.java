351139160c981602fb4b7f4ca27fb376d230b204
𝓥𝓮𝓲𝓵 <2054575@learn.vsb.bc.ca>
2026-09-16 21:12:16 -0700

    Add files via upload

--- Triangle.java
+++ Triangle.java
@@ -7,14 +7,22 @@ public class Triangle {
 	    double c = 1.0;
 	    System.out.println("Give first side length of triangle");
 	    a = scanner.nextDouble();
-	      if (a<=0.0 || b <=0.0 || c <=0.0 ) System.out.println("Side length cannot be less than or equal to 0");
+	      if (a<=0.0 || b <=0.0 || c <=0.0 ){
+			   System.out.println("Side length cannot be less than or equal to 0 --> restart");      
+			   return;
+		   }
 	    System.out.println("Give second side length of triangle");
 	    b = scanner.nextDouble();
-	     if (a<=0.0 || b <=0.0 || c <=0.0 ) System.out.println("Side length cannot be less than or equal to 0");
+	     if (a<=0.0 || b <=0.0 || c <=0.0 ) {
+	     System.out.println("Side length cannot be less than or equal to 0 --> restart");
+	     return;
+	 }
 	    System.out.println("Give third and final side length of triangle");
 	    c = scanner.nextDouble();
-	     if (a<=0.0 || b <=0.0 || c <=0.0 ) System.out.println("Side length cannot be less than or equal to 0");
-		
+	     if (a<=0.0 || b <=0.0 || c <=0.0 ) {
+			 System.out.println("Side length cannot be less than or equal to 0 --> restart");
+		return;
+	}
 		if((a+b)>c && (a+c)>b && (b+c)>a) System.out.println("A triangle can be made!");
 		else System.out.println("A triangle cannot be made!");
 		}
